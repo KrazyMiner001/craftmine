c7180c45afa380215c04ac3296a9d96df28cfd43
KrazyMiner001 <krazyminer001@outlook.com>
2025-04-05 16:34:53 +0200

    Initial commit

---
+++ src/client/java/craftmine/CraftmineClient.java
@@ -0,0 +1,10 @@
+package craftmine;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CraftmineClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
